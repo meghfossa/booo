e9a15d3f4493e9f70e9af9f099613da32345bac7
ezkangaroo <ezkangaroo@proton.me>
2025-02-01 21:52:45 -0700

    saving background for initial settings

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,1 +1,13 @@
 console.debug("background script loaded for scary tabs");
+
+import { STORAGE_KEY, defaultSettings } from "@src/settings";
+
+// Listen for extension installation
+// This is used to initialize the storage with default settings
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === "install") {
+    chrome.storage.local.set({ [STORAGE_KEY]: defaultSettings }, () => {
+      console.debug("Default settings initialized:", defaultSettings);
+    });
+  }
+});
